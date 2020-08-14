eab3674f013453c0f4d565bfffbef7bb2a0f98f8
molk95 <molk.saouabi@gmail.com>
2020-08-14 10:54:11 +0200

    first commit

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var ans = sum(5, 5);
+console.log(ans);
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+const  sum = (a: number, b: number) => {
+    return a + b
+}
+
+const ans = sum(5,5)
+console.log(ans)
+
