9ebe604a67f4d68ff49cdd4671a588b62b7f04e3
molk95 <molk.saouabi@gmail.com>
2020-08-27 12:21:13 +0200

    typescript 2

--- typescript.js
+++ typescript.js
@@ -1,5 +1,5 @@
 var sum = function (a, b) {
     return a + b;
 };
-var ans = sum(5, 5);
-console.log(ans);
+var answer = sum(5, 5);
+console.log(answer);
--- typescript.ts
+++ typescript.ts
@@ -2,6 +2,7 @@ const  sum = (a: number, b: number) => {
     return a + b
 }
 
-const ans = sum(5,5)
-console.log(ans)
+const answer = sum(5,5)
+console.log(answer
+    )
 
